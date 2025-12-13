9c9d71ae37099a5767cc91bebe5bd48adb8338ab
tmax83270-cpu <tmax83270@gmail.com>
2025-12-13 13:12:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -150,11 +150,6 @@ document.getElementById('back-to-produits').addEventListener('click', () => {
   document.getElementById('page-produits').style.display = 'block';
 });
 
-// Bouton retour vers les catégories depuis la liste de produits
-document.getElementById('back-to-categories').addEventListener('click', () => {
-  document.getElementById('page-produits').style.display = 'none';
-  document.getElementById('page-categories').style.display = 'block';
-});
 
 // --- Commander ---
 document.getElementById('order-btn').addEventListener('click', () => {
